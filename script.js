d7da5d44fe26d2fe966861a5cfbeead6c930189d
stjosephweb <atabegilbert107@gmail.com>
2026-07-16 07:01:42 +0100

    Implement back-to-top button and update welcome message
    Added a back-to-top button functionality and improved welcome message display.

--- script.js
+++ script.js
@@ -2,4 +2,19 @@ function welcomeMessage() {
     alert("Welcome to St.Joseph Pastoral Zone!");
 }
 
-document.getElementId("year").textContent= new Date().getFullYear();
+
+<!-- Back to Top Button -->
+const topBtn = document.getElementById("topBtn");
+window.onscroll = function(){
+if(document.body.scrollTop > 300 || document.documentElement.scrollTop > 300){
+topBtn.style.display="block";
+}else{
+    topBtn.style.display="none";
+}
+}
+topBtn.onclick=function(){
+    window.scrollTo({
+    top:0,
+    behavior:"smooth"
+});
+}
